fae77991972918b54528fcad406ec8705dcc0308
0fekree0 <70841845+FeKree@users.noreply.github.com>
2025-02-25 11:31:50 +0300

    1 Задание

--- LR3/1.cpp
+++ LR3/1.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+string caesarCipher(string text, int shift) {
+    string result = "";
+    int al = 32;
+    for (int i = 0; i < text.length(); i++) {
+        if (text[i] >= 'А' && text[i] <= 'Я') {
+            result += char(int(text[i] + shift - 'А') % al + 'А');
+        }
+        else if (text[i] >= 'а' && text[i] <= 'я') {
+            result += char(int(text[i] + shift - 'а') % al + 'а');
+        }
+        else {
+            result += text[i];
+        }
+    }
+    return result;
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    string text = "Шатуха Алексей Кириллович";
+    int shift = 21;
+    cout << "Оригинальный: " << text << endl;
+    cout << "Зашифрованный " << caesarCipher(text, shift) << endl;
+    return 0;
+}
